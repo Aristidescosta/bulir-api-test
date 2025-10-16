b10ea6ad3ec4efa8c7aa208c9d5c891fa821f8bb
Aristidescosta <aristidiscosta200@gmail.com>
2025-10-16 22:35:36 +0100

    chore: added code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import { server } from "./server/Server";
+import { server } from './server/Server';
+
+interface Teste {
+    
+}
 
 server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log('Server is running on http://localhost:3000');
 });
